38e4f18851bdb5f99299127446ff0d4f8be57125
Eliud Metto <eliudmetto93@gmail.com>
2022-11-11 17:07:39 +0300

    Updated index.js
    Took care of the the error handling when user's request is not successful

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Search from "./Models/Search";
 import * as searchView from "./View/searchView";
 import * as smallView from "./View/smallView";
@@ -6,7 +7,6 @@ import * as dateView from "./View/dateView";
 ///Search Controller ////
 
 const state = {};
-let cities = [];
 
 
 async function controlSearch(query) {
@@ -49,55 +49,75 @@ async function asideWeather(asideCity, num) {
 
     } catch (error) {
         alert(`Oops!! Refresh...`);
-        console.log(error);
+        smallView.loopClear();
+        init();
     }
 }
 
 
+
 async function callCities() {
 
-    if (localStorage.length > 1) {
+    if (localStorage.length > 0) {
         for (const key in localStorage) {
             if (key === 'one') {
-                await asideWeather(localStorage.getItem('one'), 'one');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('one'), 'one');
+                } else  {
+                    await asideWeather('Accra', 'one');
+                    localStorage.setItem('one', 'Accra');
+                }
             } else if (key === 'two') {
-                await asideWeather(localStorage.getItem('two'), 'two');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('two'), 'two');
+                } else  {
+                    await asideWeather('Rio', 'two');
+                    localStorage.setItem('two', 'Rio');
+                }
             } else if (key === 'three') {
-                await asideWeather(localStorage.getItem('three'), 'three');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('three'), 'three');
+                } else  {
+                    await asideWeather('London', 'three');
+                    localStorage.setItem('three', 'London');
+                }
             } else if (key === 'four') {
-                await asideWeather(localStorage.getItem('four'), 'four');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('four'), 'four');
+                } else  {
+                    await asideWeather('Delhi', 'four');
+                    localStorage.setItem('four', 'Delhi');
+                }
             } else if (key === 'five') {
-                await asideWeather(localStorage.getItem('five'), 'five');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('five'), 'five');
+                } else  {
+                    await asideWeather('Kampala', 'five');
+                    localStorage.setItem('five', 'Kampala');
+                }
             } else if (key === 'six') {
-                await asideWeather(localStorage.getItem('six'), 'six');
-                cities.push(state.aside);
+                if ( localStorage[key] !== "undefined") {
+                    await asideWeather(localStorage.getItem('six'), 'six');
+                } else  {
+                    await asideWeather('Chicago', 'six');
+                    localStorage.setItem('six', 'Chicago');
+                }
             }
         }
     }
 
     else {
         await asideWeather('Accra', 'one');
-        cities.push(state.aside);
         localStorage.setItem('one', state.aside.name);
         await asideWeather('Rio', 'two');
-        cities.push(state.aside);
         localStorage.setItem('two', state.aside.name);
         await asideWeather('London', 'three');
-        cities.push(state.aside);
         localStorage.setItem('three', state.aside.name);
         await asideWeather('Delhi', 'four');
-        cities.push(state.aside);
         localStorage.setItem('four', state.aside.name);
         await asideWeather('Kampala', 'five');
-        cities.push(state.aside);
         localStorage.setItem('five', state.aside.name);
         await asideWeather('Chicago', 'six');
-        cities.push(state.aside);
         localStorage.setItem('six', state.aside.name);
     }
 }
@@ -144,33 +164,28 @@ async function changeCity(el) {
                     smallView.clearAllUI(num);
                     await asideWeather(smallQuery, num);
                     if (num === 'one') {
-                        cities.splice(0, 1, state.aside);
                         localStorage.removeItem('one');
                         localStorage.setItem('one', state.aside.name);
                     } else if (num === 'two') {
-                        cities.splice(1, 1, state.aside);
                         localStorage.removeItem('two');
                         localStorage.setItem('two', state.aside.name);
                     } else if (num === 'three') {
-                        cities.splice(2, 1, state.aside);
                         localStorage.removeItem('three');
                         localStorage.setItem('three', state.aside.name);
                     } else if (num === 'four') {
-                        cities.splice(3, 1, state.aside);
                         localStorage.removeItem('four');
                         localStorage.setItem('four', state.aside.name);
                     } else if (num === 'five') {
-                        cities.splice(4, 1, state.aside);
                         localStorage.removeItem('five');
                         localStorage.setItem('five', state.aside.name);
                     } else if (num === 'six') {
-                        cities.splice(5, 1, state.aside);
                         localStorage.removeItem('six');
                         localStorage.setItem('six', state.aside.name);
                     }
                 }
             } catch (error) {
                 alert(`error loading request.Please try again!!`)
+                console.log(error)
             }
         }
     }
